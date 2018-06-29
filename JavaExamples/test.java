4a97802d88d5a27a2adcccfc442e31b128208234
Amir Nabaei <elhammh17@gmail.com>
2018-06-29 10:35:46 -0700

    add java examples

---
+++ JavaExamples/test.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int getTotalX(int[] a, int[] b) {
+        // Complete this function
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i = 0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int[] b = new int[m];
+        for(int b_i = 0; b_i < m; b_i++){
+            b[b_i] = in.nextInt();
+        }
+        int total = getTotalX(a, b);
+        System.out.println(total);
+        in.close();
+    }
+}
--- src/hotdog.java
+++
@@ -1,8 +0,0 @@
-
-public class hotdog extends Food{
-	
-//  void eat(){
-//	  System.out.println("This is hotdog class");
-//  }
-
-}
