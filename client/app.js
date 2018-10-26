90af1c6c2baf8ef08e341d759bf0c03939f9eb53
Jordan Toering <toering3@gmail.com>
2018-10-26 14:39:25 -0700

    sub titles

--- client/app.js
+++ client/app.js
@@ -1,6 +1,25 @@
 var app = angular.module("dmApp", []);
 
 app.controller('MainController', ['$scope', function($scope) {
+    
+    const reasonTemplate = {
+        id: "",
+        alignment: "",
+        reasonText: "",
+        strength: 0,
+        evidences: []
+    };
+
+    $scope.mapState = {
+        assignment: "Assignment 1",
+        topic: "Are Dialectical maps an effective teaching tool?",
+        ownerID: "jtoering",
+        reasons: []
+    };
+
+    $scope.prebuild = function() {
+        console.log("ng-init ran");
+    };
 
     $scope.redo = function() {
         console.log("Redo function");
