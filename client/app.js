68b9e64e7c6ba58fa41ea4d5e8fefd5f19c47467
Jordan Toering <toering3@gmail.com>
2018-10-26 12:46:56 -0700

    Top bar complete, stub functions for buttons

--- client/app.js
+++ client/app.js
@@ -1,5 +1,21 @@
 var app = angular.module("dmApp", []);
 
 app.controller('MainController', ['$scope', function($scope) {
+
+    $scope.redo = function() {
+        console.log("Redo function");
+    };
+
+    $scope.submit = function() {
+        console.log("submit function");
+    }
+
+    $scope.undo = function() {
+        console.log("undo function");
+    }
+
+    $scope.download = function() {
+        console.log("download function");
+    }
     
 }]);
