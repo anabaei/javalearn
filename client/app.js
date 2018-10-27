d2a0bec80a866dfa958b10ef6cf7fbc9b65746ea
Jordan Toering <toering3@gmail.com>
2018-10-26 18:00:31 -0700

    partial client file progress on reason components

--- client/app.js
+++ client/app.js
@@ -2,23 +2,59 @@ var app = angular.module("dmApp", []);
 
 app.controller('MainController', ['$scope', function($scope) {
     
+    const evidenceTemplate = {
+        id: null,
+        supports: true,
+        evidenceText: "",
+        warrantText: ""
+    };
+
     const reasonTemplate = {
-        id: "",
-        alignment: "",
+        id: null,
+        linkID: null,
         reasonText: "",
-        strength: 0,
+        strength: 1,
         evidences: []
     };
 
-    $scope.mapState = {
-        assignment: "Assignment 1",
-        topic: "Are Dialectical maps an effective teaching tool?",
-        ownerID: "jtoering",
-        reasons: []
+    const mapTemplate = {
+        id: null,
+        ownerID: null,
+        assignment: "",
+        topic: "",
+        stanceBlue: "",
+        blueReasons: [],
+        stanceYellow: "",
+        yellowReasons: [],
+        conclusion: ""
     };
 
+    $scope.mapState = {};
+
     $scope.prebuild = function() {
-        console.log("ng-init ran");
+
+        //Instantiate a new map
+        $scope.mapState = _.cloneDeep(mapTemplate);
+        $scope.mapState.assignment = "Assignment 1";
+        $scope.mapState.topic = "Are dialectical maps an effective teaching tool?";
+        $scope.mapState.stanceBlue = "Pro/Agree";
+        $scope.mapState.stanceYellow = "Con/Disagree";
+        $scope.mapState.conclusion = "Lorem ipsums";
+
+        var firstReason = _.cloneDeep(reasonTemplate);
+        firstReason.id = 0;
+
+        var secondReason = _.cloneDeep(reasonTemplate);
+        secondReason.id = 1;
+        
+        $scope.mapState.blueReasons.push(firstReason);
+        $scope.mapState.yellowReasons.push(secondReason);
+
+        $scope.logMapState();
+    };
+
+    $scope.logMapState = function() {
+        console.log($scope.mapState);
     };
 
     $scope.redo = function() {
