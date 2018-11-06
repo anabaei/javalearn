8013e11e12d856d303944f9384ae08e4118721b5
Jordan Toering <toering3@gmail.com>
2018-11-05 17:58:32 -0800

    beginning linkage of data into components

--- client/app.js
+++ client/app.js
@@ -1,64 +1,29 @@
 var app = angular.module("dmApp", []);
 
-app.controller('MainController', ['$scope', function($scope) {
-    
-    const evidenceTemplate = {
-        id: null,
-        supports: true,
-        evidenceText: "",
-        warrantText: ""
-    };
-
-    const reasonTemplate = {
-        id: null,
-        linkID: null,
-        reasonText: "",
-        strength: 1,
-        evidences: []
-    };
-
-    const mapTemplate = {
-        id: null,
-        ownerID: null,
-        assignment: "",
-        topic: "",
-        stanceBlue: "",
-        blueReasons: [],
-        stanceYellow: "",
-        yellowReasons: [],
-        conclusion: ""
-    };
+app.controller('MainController', ['$scope', '$http', function($scope,$http) {
 
     $scope.mapState = {};
 
-    $scope.strengthValues = [
-        1,
-        2,
-        3,
-        4,
-        5
-    ];
+    $scope.possibleStrengthValues = [1,2,3,4,5];
 
-    $scope.prebuild = function() {
+    $scope.initialize = function() {
 
-        //Instantiate a new map
-        $scope.mapState = _.cloneDeep(mapTemplate);
-        $scope.mapState.assignment = "Assignment 1";
-        $scope.mapState.topic = "Are dialectical maps an effective teaching tool?";
-        $scope.mapState.stanceBlue = "Pro/Agree";
-        $scope.mapState.stanceYellow = "Con/Disagree";
-        $scope.mapState.conclusion = "Lorem ipsums lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems lorems";
+        //get the sample data via http get
+        $http.get('prototypeMap.json')
+        .then(function buildInitialState(response) {
+            $scope.mapState = response.data;
+            console.log("Sample data loaded");
+        }, function notifyInitialStateFailure(response) {
+            console.log(response);
+        });
+    };
 
-        var firstReason = _.cloneDeep(reasonTemplate);
-        firstReason.id = 0;
+    $scope.blueFilter = function(item) {
+        return item.isBlue;
+    };
 
-        var secondReason = _.cloneDeep(reasonTemplate);
-        secondReason.id = 1;
-        
-        $scope.mapState.blueReasons.push(firstReason);
-        $scope.mapState.yellowReasons.push(secondReason);
-
-        $scope.logMapState();
+    $scope.yellowFilter = function(item) {
+        return !item.isBlue;
     };
 
     $scope.blueToggle = true;
