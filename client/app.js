6ca206b6776a0fc5ac1a281b715cc21c09d28062
Jordan Toering <toering3@gmail.com>
2018-11-21 11:24:54 -0800

    move autogrow inside of app file to ensure it loads first

--- client/app.js
+++ client/app.js
@@ -1,5 +1,69 @@
 var app = angular.module("dmApp", ['angular.filter']);
 
+(function(main, $) {
+  
+    "use strict";
+    
+    main
+      .directive("textarea", function() {
+        return {
+          restrict: "E",
+          require: "ngModel",
+          link: AutogrowLink
+        };
+      });
+      
+    function AutogrowLink(scope, element, attributes) {
+      if (!element.hasClass("autogrow")) {
+        // no autogrow for you today
+        return;
+      }
+  
+      // get possible minimum height style
+      var minHeight = parseInt(window.getComputedStyle(element[0]).getPropertyValue("min-height")) || 0;
+  
+      // prevent newlines in textbox
+      element.on("keydown", function(evt) {
+        if (evt.which === 13)
+        {
+          evt.preventDefault();
+        }
+      }
+      );
+    
+      
+      element.on("input" , function(evt) {
+        element.css({
+          paddingTop: 0,
+          height: 0,
+          minHeight: 0
+        });
+        
+        var contentHeight = this.scrollHeight;
+        var borderHeight = this.offsetHeight;
+  
+        element.css({
+          //paddingTop: ~~Math.max(0, minHeight - contentHeight) / 2 + "px",
+          paddingTop: "1vw",
+          paddingBottom: "1vw",
+          minHeight: null, // remove property
+          height: contentHeight + borderHeight + "px" // because we're using border-box
+        });
+      });
+  
+      // watch model changes from the outside to adjust height
+      scope.$watch(attributes.ngModel, trigger);
+      
+      // set initial size
+      trigger();
+      
+      function trigger() {
+        setTimeout(element.triggerHandler.bind(element, "input"), 1);
+      }
+    }
+    
+  })(app, angular.element);
+
 app.controller('MainController', ['$scope', '$http', function($scope,$http) {
 
     const reasonTemplate = {
@@ -345,7 +409,18 @@ app.controller('MainController', ['$scope', '$http', function($scope,$http) {
     };
 
     $scope.download = function() {
-        console.log("download function");
+        //build the content TODO
+        var mapString = "sample text content\n\n\nsample\n\nsample";
+
+        var link = document.createElement('a');
+        link.download = "DM-" + $scope.mapState.mapID + ".txt";
+        var blob = new Blob([mapString], {type: 'text/plain'});
+        link.href = window.URL.createObjectURL(blob);
+
+        //trigger the download
+        //link.click();
+        console.log(mapString);
+
     };
 
     $scope.unlinkArguments = function(linkedBlueReason,linkedYellowReason) {
