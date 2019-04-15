215191c6a9c6becc01e75d5c25fef69d2e4b58ff
Amir Nabaei <nabaei17@gmail.com>
2019-04-15 13:22:53 -0700

    add java test

---
+++ AmirtestJava/src/Amir.java
@@ -0,0 +1,7 @@
+
+public class Amir {
+
+	public Amir() {
+		System.out.println("amir1");
+	}
+}
---
+++ AmirtestJava/src/testmain.java
@@ -0,0 +1,12 @@
+
+public class testmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.print("SS");
+       Amir a = new Amir();
+	}
+	
+}
+
+
